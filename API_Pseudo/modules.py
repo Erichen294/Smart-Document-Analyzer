ea02308f811657ce5ac5e15f97350fb81f65fae4
Erichen294 <chene@bu.edu>
2024-02-11 13:39:32 -0500

    Created API module function stubs

---
+++ API_Pseudo/modules.py
@@ -0,0 +1,27 @@
+## Secure File Uploader/Ingester and Text NLP Analysis ##
+
+def fileUpload(file, api_key):
+    if authenticate(api_key) == "success":
+        if file == "success":
+            print("File upload success")
+            return "success"
+        else:
+            print("File upload fail")
+            return "Failed" 
+
+def authenticate(api_key):
+    if api_key == "match":
+        return "success"
+    return "failed"
+
+def textAnalysis(text):
+    # Tokening strings
+    tokens = tokenize(text)
+
+    # Checking frequency
+    frequency = countFreq(tokens)
+
+    # Analysis
+    analysis = analyze(tokens)
+
+    return analysis
