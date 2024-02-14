ebf62ee623536eeb563f7faac9951f4600f07b64
Eric Chen <98416392+Erichen294@users.noreply.github.com>
2024-02-13 22:44:32 -0500

    Added more comments

--- API_Pseudo/uploader.py
+++ API_Pseudo/uploader.py
@@ -2,26 +2,35 @@
 
 def fileUpload(file, api_key):
     if authenticate(api_key) == "success":
+        # Authenticating user
         if identifyFileType(file) == "success":
+            # File type is valid
             print("File upload success")
             return "success"
         else:
+            # File type is invalid
             print("File upload fail")
             return "failed" 
 
 def authenticate(api_key):
     if api_key == "match":
+        # API key valid 
         return "success"
+    # API key invalid
     return "failed"
 
 def login(username, password, api_key):
     if username == "match" and password == "match" and authenticate(api_key) == "success":
+        # User successfully logs in
         print("Login successful")
         return "success"
+    # User fails to log in
     print("Login failed")
     return "failed"
 
 def identifyFileType(file):
     if fileType == "valid":
+        # Returns file type and success if file type is valid
         return "success", fileType
+    # File type is invalid
     return "failed"
